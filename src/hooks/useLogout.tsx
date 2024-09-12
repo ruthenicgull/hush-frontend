91e01dd1e31a8cd9d864b8d0f286e811f05f169e
ruthenicgull <muhammedhisham2003@gmail.com>
2024-09-13 04:05:39 +0530

    created a logout hook

---
+++ src/hooks/useLogout.tsx
@@ -0,0 +1,41 @@
+import axios from "@/api/axios";
+import { logoutUser, selectAccessToken } from "@/features/user/userSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { useState } from "react";
+
+function useLogout() {
+  const [error, setError] = useState<string | null>(null);
+  const dispatch = useDispatch();
+  const accessToken = useSelector(selectAccessToken);
+
+  const logout = async () => {
+    dispatch(logoutUser()); // Remove user data from the store
+    setError(null); // Reset error state on successful logout
+
+    try {
+      await axios.post(
+        "user/logout",
+        {},
+        {
+          withCredentials: true,
+          headers: { Authorization: `Bearer ${accessToken}` },
+        }
+      );
+    } catch (error: any) {
+      if (
+        error.response &&
+        error.response.data &&
+        error.response.data.message
+      ) {
+        setError(error.response.data.message);
+      } else {
+        setError("An unexpected error occurred. Please try again.");
+      }
+      console.log("Failed to log out", error);
+    }
+  };
+
+  return [logout, error] as const;
+}
+
+export default useLogout;
