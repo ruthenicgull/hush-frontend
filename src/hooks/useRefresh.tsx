aa26b4d1ab3eceed062e8007d4702b29088f254f
ruthenicgull <muhammedhisham2003@gmail.com>
2024-09-13 04:06:17 +0530

    updated useRefresh with new userSlice structure

--- src/hooks/useRefresh.tsx
+++ src/hooks/useRefresh.tsx
@@ -1,26 +1,33 @@
 import axios from "@/api/axios";
 import { useDispatch } from "react-redux";
-import { userSlice } from "@/features/user/userSlice";
+import { setUser } from "@/features/user/userSlice";
 
 const useRefreshToken = () => {
   const dispatch = useDispatch();
 
   const refresh = async () => {
-    const response = await axios.post(
-      "/user/refresh-access-token",
-      {}, // Request body (empty in this case)
-      {
-        withCredentials: true,
-      }
-    );
+    try {
+      const response = await axios.post(
+        "/user/refresh-access-token",
+        {}, // Request body (empty in this case)
+        {
+          withCredentials: true,
+        }
+      );
 
-    dispatch(
-      userSlice.actions.setUser({
-        user_id: response?.data?.data?.user_id,
-        refreshToken: response?.data?.data?.refreshToken,
-        accessToken: response?.data?.data?.accessToken,
-      })
-    );
+      dispatch(
+        setUser({
+          user_id: response?.data?.data?.user?._id,
+          username: response?.data?.data?.user?.username,
+          email: response?.data?.data?.user?.email,
+          accessToken: response?.data?.data?.accessToken,
+          refreshToken: response?.data?.data?.refreshToken,
+          isAuthenticated: true,
+        })
+      );
+    } catch (error) {
+      console.error("Failed to refresh access token:", error);
+    }
   };
 
   return refresh;
